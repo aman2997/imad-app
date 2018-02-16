7c4d98159f7e8dd80574920ec5cc7d506de0e86e
aman2997 <amansrivastav9a33@gmail.com>
2018-02-16 18:09:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,46 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var contentone = {
+  title: 'Article-one',
+  date: '5th Feb, 2018',
+  content: 
+                `<p>
+                    This is the content for my first article. This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                </p>
+                <p>
+                    This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                </p>`
+};
+
+
+var htmltemplate =
+   ` <html>
+    <head>
+        <title>$(title)</title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="k">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr>
+            <div>
+                $(date)
+            </div>
+            <div>
+                $(content)
+            </div>
+        </div>
+    </body>
+</html>
+`;
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
