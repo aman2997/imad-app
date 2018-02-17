91c992a6e53cecbe9bf29899bdb7a2c6bc64d732
aman2997 <amansrivastav9a33@gmail.com>
2018-02-17 12:25:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,4 @@
 console.log('Loaded!');
-
-//change text of abc div
-
-var element = document.getElementById('abc');
-element.innerHTML = 'Why does Aman keep changing this text with JavaScript';
-
 //move image
 
 var img = document.getElementById('m');
