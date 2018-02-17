909731a5b66d94e4540ce32c0c460272468adbd0
aman2997 <amansrivastav9a33@gmail.com>
2018-02-17 11:45:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,11 @@ console.log('Loaded!');
 
 var element = document.getElementById('abc');
 element.innerHTML = 'Why does Aman keep changing this text with JavaScript';
+
+//move image
+
+var ele = document.getElementById('m');
+m.onclick = function()
+{
+    img.style.marginLeft = '100px';
+};
