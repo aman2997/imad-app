3a2b695aab0fc88ff910f922f7f0d29db993e63e
aman2997 <amansrivastav9a33@gmail.com>
2018-02-17 12:33:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,16 +16,22 @@ img.onclick = function()
     
 };
 
-var counter = 0;
 var button = document.getElementById('c');
 button.onclick = function() {
     //Make a request to the counter endpoint
+    var request = new XMLhttpRequest();
     
     //Capture the response and store it in a variable
+    request.onreadystatechange  = function() {
+        if(request.readyState === XMLhttpRequest.DONE ) {
+            //Take some action
+            if (request.status === 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+            }
+        }
+        //Not done yet
+    };
     
-    //Render the variable in the cprrect span
-    
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
 };
