a858256ec50e9dab6445eb29e6282a2f87a465f7
aman2997 <amansrivastav9a33@gmail.com>
2018-02-25 20:45:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,9 +123,26 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/:articleName', function (req, res) {
-    var articleName = req.params.articleName;
-  res.send(createTemplate(article[articleName]));
+app.get('/articles/:articleName', function (req, res) {
+
+    pool.query("SELECT * fROM test WHERE article = " + req.params.articleName;, function (err, result) {
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            if(result.rows.length == 0)
+            {
+                res.status(404).send('Article not found');
+            }
+            else
+            {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+            }
+        }
+    })
 });
 
 
