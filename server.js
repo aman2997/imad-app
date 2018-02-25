03579436273b26e7fd8a4ec35cad1848ca83ed10
aman2997 <amansrivastav9a33@gmail.com>
2018-02-25 20:53:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/articles/:articleName', function (req, res) {
 
-    pool.query("SELECT * FROM test WHERE title = '" + req.params.articleName + "'", function (err, result) {
+    pool.query("SELECT * FROM test WHERE id = '" + req.params.articleName + "'", function (err, result) {
         if(err)
         {
             res.status(500).send(err.toString());
