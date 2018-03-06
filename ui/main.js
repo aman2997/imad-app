6fe91747367706f0da3fe642ef24da4522790f7b
aman2997 <amansrivastav9a33@gmail.com>
2018-03-06 18:00:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,3 @@
-console.log('Loaded!');
-
-
-var button = document.getElementById('c');
-
-button.onclick = function() {
-    //Make a request to the counter endpoint
-    var request = new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    request.onreadystatechange  = function() {
-        if(request.readyState === XMLHttpRequest.DONE ) {
-            //Take some action
-            if (request.status === 200) {
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-            }
-        }
-        //Not done yet
-    };
-    //Make the request
-    request.open('GET','http://amansrivastav9a33.imad.hasura-app.io/counter',true);
-    request.send(null);
-    
-};
-
-
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
      var request = new XMLHttpRequest();
@@ -35,24 +7,23 @@ submit.onclick = function() {
         if(request.readyState === XMLHttpRequest.DONE ) {
             //Take some action
             if (request.status === 200) {
-              var names = request.responseText;
-              names = JSON.parse(names);
-              var list = '';
-              for (var i = 0; i<names.length; i++)
-              {
-                  list += '<li>' + names[i] + '</li>';
-              }
-              var ul = document.getElementById('namelist');
-              ul.innerHTML = list;
+              alert('Logged in');
+            }
+            else if(request.status === 403) {
+                alert('Incorrect usernameor password');
+            }
+            else if(request.status === 500) {
+                alert('Something went wrong');
             }
         }
         //Not done yet
     };
     //Make the request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET','http://amansrivastav9a33.imad.hasura-app.io/submit-name?name=' + name,true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    request.open('POST','http://amansrivastav9a33.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));
     
   //Make a request to the server and send the name
   //Capture a list of names and render it as a list
