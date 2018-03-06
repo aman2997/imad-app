0247705206ebde1034bd74a35db3e8bbd0dccf00
aman2997 <amansrivastav9a33@gmail.com>
2018-03-06 16:22:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ return htmltemplate;
 function hash (input, salt) {
     //how to create a hash
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex');
+    return ["pbkdf2Sync", "10000", salt, hashed.toString('hex')].join('$');
 }
 
 
